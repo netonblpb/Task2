77ef53e9002e469757b021e4850862eb0a6a44f2
netonblpb <skullbog@gmail.com>
2022-02-03 13:07:25 +0300

    Task2 Complete

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// Task2: Даны два числа. Показать большее и меньшее число
+
+int numberOne = 9;
+int numberTwo = 7;
+
+if (numberOne > numberTwo)
+Console.WriteLine(numberOne + " больше " + numberTwo);
+else
+Console.WriteLine(numberTwo + " больше " + numberOne);
